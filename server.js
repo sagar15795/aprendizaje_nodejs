0d556f6f179677e2935fecd951d9b02239e7cd14
Sagar <kumarsagar15795@gmail.com>
2017-10-01 00:30:09 +0530

    change request handler for routing

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for "+pathname+" received.");
-    route(handle, pathname);
+    var content = route(handle, pathname);
     response.writeHead(200, {
       "Content-Type": "text/plain"
     });
-    response.write("Hello World");
+    response.write(content);
     response.end();
   }
   http.createServer(onRequest).listen(8888);
