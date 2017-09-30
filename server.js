9d487fd81d8cdb6f7fa1f8043fcf3c2941101434
Sagar <kumarsagar15795@gmail.com>
2017-10-01 00:42:06 +0530

    update code for non blocking task

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for "+pathname+" received.");
-    var content = route(handle, pathname);
-    response.writeHead(200, {
-      "Content-Type": "text/plain"
-    });
-    response.write(content);
-    response.end();
+    var content = route(handle, pathname,response);
   }
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
